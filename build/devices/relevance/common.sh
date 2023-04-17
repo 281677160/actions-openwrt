8dc551b1281c400e9e2d21b7f5c19a9c4b90d695
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-17 13:11:48 +0800

    Update common.sh

--- build/devices/relevance/common.sh
+++ build/devices/relevance/common.sh
@@ -272,7 +272,7 @@ echo -e "\033[32m 应用文件: ${FOLDER_NAME} \033[0m"
 echo -e "\033[32m 配置文件: $(echo "${CONFIG_FILE}" |cut -d"/" -f2) \033[0m"
 echo -e "\033[32m 扩展文件: ${DIY_PART_SH} \033[0m"
 echo -e "\033[32m 编译机型: ${TARGET_PROFILE} \033[0m"
-echo -e "\033[32m 固件后台IP: ${IPV4_IPADDR} \033[0m"
+echo -e "\033[32m 固件后台IP: ${TEMPOARY_IP} \033[0m"
 if [[ "${PACKAGING_FIRMWARE}" == "true" ]] && [[ "${TARGET_PROFILE}" == "Armvirt_64" ]]; then
   echo -e "\033[32m 内核版本: 以打包内核为准 \033[0m"
   echo
