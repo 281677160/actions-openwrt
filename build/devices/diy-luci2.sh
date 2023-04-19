089b87b1dc6041c6cfa3f76babf184a37356d78b
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-20 01:06:15 +0800

    Update diy-luci2.sh

--- build/devices/diy-luci2.sh
+++ build/devices/diy-luci2.sh
@@ -5,16 +5,7 @@
 # diy-luci2.sh  LUCI2源码使用
 #======================================================
 
-#1. 首次进入固件免密登录设置(填1为启用,0为不启用)
-export DELETE_LOGIN_PASSWORD="1"
-
-#2. 清理[X]天之前的Artifacts
-export RETAIN_DAYS="7"
-
-#3. 保留[X]个Releases不被删除
-export KEEP_LATEST="10"
-
-#4. 晶晨CPU系列打包固件设置(不懂请看说明)
+# 晶晨CPU系列打包固件设置(不懂请看说明)
 export amlogic_model="s905d"
 export amlogic_kernel="5.10.01_6.1.01"
 export auto_kernel="true"
