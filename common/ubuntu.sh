acab20816bf95d65e3f50d66cac710a7e05b59d7
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-20 20:58:41 +0800

    Update ubuntu.sh

--- common/ubuntu.sh
+++ common/ubuntu.sh
@@ -14,8 +14,6 @@ sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /
 function install_mustrelyon(){
 # 安装我仓库需要的依赖
 sudo apt-get install -y rename pigz libfuse-dev
-git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
 
 # 安装天灵大佬的依赖
 sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
