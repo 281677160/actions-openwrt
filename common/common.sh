b190e0b9d1237629d3f2aa93bed6d687987ec405
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-20 18:15:37 +0800

    Update common.sh

--- common/common.sh
+++ common/common.sh
@@ -81,7 +81,7 @@ fi
 settingss="$(find "${HOME_PATH}/package" -type d -name "default-settings")"
 if [[ ! -d "${settingss}" ]] && [[ "${DIY_PART_SH}" == "diy-luci2.sh" ]]; then
   svn export https://github.com/281677160/common/trunk/OFFICIAL/default-settings ${HOME_PATH}/package/default-settings > /dev/null 2>&1
-  [[ ! -d "${HOME_PATH}/feeds/luci/libs/luci-lib-base" ]] && svn export https://github.com/openwrt/luci/branches/openwrt-22.03/libs/luci-lib-base ${HOME_PATH}/feeds/luci/libs/luci-lib-base > /dev/null 2>&1
+  [[ ! -d "${HOME_PATH}/feeds/luci/libs/luci-lib-base" ]] && sed -i "s/+luci-lib-base //g" ${HOME_PATH}/package/default-settings/Makefile
 elif [[ ! -d "${settingss}" ]] && [[ "${DIY_PART_SH}" == "diy-luci1.sh" ]]; then
   svn export https://github.com/281677160/common/trunk/COOLSNOWWOLF/default-settings ${HOME_PATH}/package/default-settings > /dev/null 2>&1
 fi
