1432b1c7867ea49674d9dd8b6f0f5e107473d7f0
281677160 <66651997+281677160@users.noreply.github.com>
2023-04-20 13:18:29 +0800

    Update common.sh

--- common/common.sh
+++ common/common.sh
@@ -38,6 +38,17 @@ echo "DIY_WORK=${DIY_WORK}" >> $GITHUB_ENV
 
 
 function Package_settings() {
+cd ${GITHUB_WORKSPACE}/openwrt
+case "${REPO_URL}" in
+https://github.com/openwrt/openwrt)
+  if [[ "${REPO_BRANCH}" =~ (openwrt-19.07|openwrt-21.02|openwrt-22.03) ]]; then
+    export LUCI_CHECKUT="$(git tag| awk 'END {print}')"
+    git checkout ${LUCI_CHECKUT}
+    git switch -c ${LUCI_CHECKUT}
+  fi
+;;
+esac
+
 export HOME_PATH="$GITHUB_WORKSPACE/openwrt"
 cp -Rf $GITHUB_WORKSPACE/build/${FOLDER_NAME} ${HOME_PATH}/build
 cp -Rf $GITHUB_WORKSPACE/common/*.sh ${HOME_PATH}/build/
@@ -45,9 +56,6 @@ sudo chmod -R +x ${HOME_PATH}/build
 echo "HOME_PATH=${HOME_PATH}" >> $GITHUB_ENV
 echo "BUILD_PATH=${HOME_PATH}/build" >> $GITHUB_ENV
 cd ${HOME_PATH}
-export LUCI_CHECKUT="$(git tag| awk 'END {print}')"
-git checkout ${LUCI_CHECKUT}
-git switch -c ${LUCI_CHECKUT}
 ./scripts/feeds update -a > /dev/null 2>&1
 
 sed -i 's/root:.*/root::0:0:99999:7:::/g' ${HOME_PATH}/package/base-files/files/etc/shadow
