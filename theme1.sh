606c224f79349dbee48a2f356958ee22b352691c
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-01 14:57:46 +0800

    Update theme1.sh

--- theme1.sh
+++ theme1.sh
@@ -21,33 +21,6 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns smartdns
 
 
-luci_file="$(grep -Eio "<%= ver.luciname %>"  -rl ./ |grep "\.htm")"
-for x in ${luci_file}
-do
-  a="$(grep -Eio 'https://github.com/Lienol/openwrt-luci' "$x")"
-  if [[ -n "${a}" ]]; then
-    sed -i "s?${a}?/cgi-bin/luci/admin/status/overview?g" "$x"
-  else
-    a="$(grep -Eio 'https://github.com/openwrt/luci' "$x")"
-    [[ -n "${a}" ]] && sed -i "s?${a}?/cgi-bin/luci/admin/status/overview?g" "$x"
-  fi
-  
-  b="$(grep -Eio 'Powered.*<%= ver.luciname %>' "$x")"
-  [[ -n "${b}" ]] && sed -i "s?${b}??g" "$x"
-  
-  c="$(grep -Eio '\(<%= ver.luciversion %>\)</a> /' "$x")"
-  if [[ -n "${c}" ]]; then
-    sed -i "s?${c}??g" "$x"
-    sed -i 's?<%= ver.distversion %>?<%= ver.distversion %> </a>?g' "$x"
-  else
-    [[ -z "$(grep "<%= ver.distversion %>" "$x")" ]] && sed -i 's?<%= ver.luciversion %>?<%= ver.distversion %>?g' "$x"
-  fi
-  
-  d="$(grep -Eio '<a href="https://github.com/.*%></a> /' "$x")"
-  [[ -n "${d}" ]] && sed -i "s?${d}??g" "$x"
-done
-
-
 ## 生成完整目录清单
 cat > Update.txt <<EOF
 luci-app-argon-config
