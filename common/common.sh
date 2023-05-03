35fe0e67db04d3dec58a83d9231aaa8754274c04
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-03 13:35:46 +0800

    Update common.sh

--- common/common.sh
+++ common/common.sh
@@ -317,7 +317,6 @@ fi
 echo
 echo -e "\033[32m 源码链接: ${REPO_URL} \033[0m"
 echo -e "\033[32m 源码分支: ${REPO_BRANCH} \033[0m"
-echo -e "\033[32m 扩展文件: ${DIY_PART_SH} \033[0m"
 echo -e "\033[32m 配置文件: $(echo "${CONFIG_FILE}" |cut -d"/" -f2) \033[0m"
 echo -e "\033[32m 编译机型: ${TARGET_PROFILE} \033[0m"
 echo -e "\033[32m 固件后台IP: ${TEMPOARY_IP} \033[0m"
