c34e8e84703c6e70599a5b894041a289da952f20
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-03 12:11:19 +0800

    Update and rename diy-luci1.sh to diy-part.sh

---
+++ build/devices/diy-part.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#======================================================
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# DIY扩展二合一了，在此处可以增加插件,可修改IP,修改源码
+#======================================================
+
+# 晶晨CPU系列打包固件设置(不懂请看说明)
+export amlogic_model="s905d"
+export amlogic_kernel="5.10.01_6.1.01"
+export auto_kernel="true"
+export rootfs_size="2560"
+export kernel_usage="stable"
