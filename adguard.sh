a5fc417b2c8d412c82b0cb771f7102063e3b1661
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-20 11:46:16 +0800

    Delete adguard.sh

--- adguard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# https://github.com/281677160/AutoBuild-OpenWrt
-# common Module by 28677160
-
-
-## sirpdboy 插件
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome luci-app-adguardhome
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome adguardhome
