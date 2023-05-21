63e0ac571815edf70ce3b4d36a1ba9e9f1723dba
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-21 18:53:12 +0800

    Update diy-part.sh

--- build/devices/diy-part.sh
+++ build/devices/diy-part.sh
@@ -19,6 +19,7 @@ export kernel_usage="stable"
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 packages
+profiles.json
 config.buildinfo
 feeds.buildinfo
 sha256sums
