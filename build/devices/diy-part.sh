9a55adf4ffbf34db60f973dbfbfacaa6112fcfd4
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-21 11:30:23 +0800

    Update diy-part.sh

--- build/devices/diy-part.sh
+++ build/devices/diy-part.sh
@@ -10,3 +10,15 @@ export amlogic_kernel="5.10.01_6.1.01"
 export auto_kernel="true"
 export rootfs_size="2560"
 export kernel_usage="stable"
+
+# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
+cat >"$CLEAR_PATH" <<-EOF
+packages
+config.buildinfo
+feeds.buildinfo
+openwrt-x86-64-generic-kernel.bin
+openwrt-x86-64-generic.manifest
+openwrt-x86-64-generic-squashfs-rootfs.img.gz
+sha256sums
+version.buildinfo
+EOF
