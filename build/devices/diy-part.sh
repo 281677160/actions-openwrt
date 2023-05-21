d1b6980e8aad5630922556e17f8af7c9887111cd
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-21 11:31:34 +0800

    Update diy-part.sh

--- build/devices/diy-part.sh
+++ build/devices/diy-part.sh
@@ -21,9 +21,9 @@ cat >"$CLEAR_PATH" <<-EOF
 packages
 config.buildinfo
 feeds.buildinfo
+sha256sums
+version.buildinfo
 openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
-sha256sums
-version.buildinfo
 EOF
