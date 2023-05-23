bfc110033ba62264966bb45b8ae74c3e25b3690c
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-24 00:58:46 +0800

    Update diy-part1.sh

--- build/devices/diy-part1.sh
+++ build/devices/diy-part1.sh
@@ -4,3 +4,6 @@
 # 自定义文件1（diy-part1.sh）
 # 在此处只能使用增加插件的命令，不能使用修改源码命令
 #======================================================
+
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/luci-app-msd_lite
