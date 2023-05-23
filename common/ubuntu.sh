eee7256fcf1f67e64a8f3d67c1d90f31368f4612
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-23 22:06:51 +0800

    Update ubuntu.sh

--- common/ubuntu.sh
+++ common/ubuntu.sh
@@ -3,9 +3,7 @@
 
 function Delete_useless(){
 # 删除一些不需要的东西
-docker rmi `docker images -q`
-sudo -E apt-get -qq remove -y --purge azure-cli ghc* zulu* llvm* firefox google* powershell openjdk* msodbcsql17 mongodb* moby* snapd* mysql*
-sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc /swapfile
+echo
 }
 
 function install_mustrelyon(){
@@ -14,6 +12,11 @@ systemctl daemon-reload
 sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh)'
 systemctl daemon-reload
 sudo apt-get install -y rename pigz libfuse-dev
+
+docker rmi `docker images -q`
+sudo -E apt-get -qq remove -y --purge azure-cli ghc* zulu* llvm* firefox google* powershell openjdk* msodbcsql17 mongodb* moby* snapd* mysql*
+sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc /swapfile
+
 sudo apt-get install -y $(curl -fsSL https://is.gd/depend_ubuntu2204_openwrt)
 }
 
