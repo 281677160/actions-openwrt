b7f4df8f244eba55f65dd6b02e88d0f7c186e47c
281677160 <66651997+281677160@users.noreply.github.com>
2023-05-24 17:35:41 +0800

    Delete diy-part1.sh

--- build/devices/diy-part1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#======================================================
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-# 自定义文件1（diy-part1.sh）
-# 在此处只能使用增加插件的命令，不能使用修改源码命令
-#======================================================
-
-git clone https://github.com/ximiTech/msd_lite.git package/msd_lite
-git clone https://github.com/ximiTech/luci-app-msd_lite.git package/luci-app-msd_lite
