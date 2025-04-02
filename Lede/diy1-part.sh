9831677430c258a6611a959475084acaf999a7ca
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-02 20:20:02 +0800

    Update diy1-part.sh

--- Lede/diy1-part.sh
+++ Lede/diy1-part.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
-#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone --depth 1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
