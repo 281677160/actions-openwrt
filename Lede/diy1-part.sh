645c0765ca65684b2753f9be0bd257359602ed86
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-02 20:27:11 +0800

    Update diy1-part.sh

--- Lede/diy1-part.sh
+++ Lede/diy1-part.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
-#git clone --depth 1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-#git clone --single-branch --depth=1 --branch=main https://github.com/xiaorouji/openwrt-passwall-packages  package/passwall-packages
+git clone --depth 1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --single-branch --depth=1 --branch=main https://github.com/xiaorouji/openwrt-passwall-packages  package/passwall-packages
