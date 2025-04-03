3e07e4990ded07c11468b1709b38cfcc47f8e183
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-03 18:33:58 +0800

    Update diy1-part.sh

--- Lede/diy1-part.sh
+++ Lede/diy1-part.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld;master' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> feeds.conf.default
 echo 'src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages;main' >> feeds.conf.default
