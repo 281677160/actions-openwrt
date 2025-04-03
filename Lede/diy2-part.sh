d7be2ec6dc813f86198d43b365c881bd19f57244
281677160 <66651997+281677160@users.noreply.github.com>
2025-04-03 18:53:53 +0800

    Update diy2-part.sh

--- Lede/diy2-part.sh
+++ Lede/diy2-part.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
+# 修改必选主题
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 # 设置 IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 # 设置主机名称
-sed -i 's/OpenWrt/OpenWrt123/g' package/base-files/files/bin/config_generate
-
-# 修改必选主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/OpenWrt/OpenWrt123/g' package/base-files/files/bin/config_generate
